90af17d138f8151d09c76aff9613dba8ca00ee5f
iqro banyuanto <iqrobanyuanto@kata.ai>
2025-02-13 19:13:20 +0700

    add agent workflow

--- agents.py
+++ agents.py
@@ -2,7 +2,7 @@ import os
 from qdrant_client import QdrantClient
 from sentence_transformers import SentenceTransformer
 
-from autogen import AssistantAgent
+from autogen import Agent, AssistantAgent, GroupChat, GroupChatManager, UserProxyAgent
 from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
 
 config = {
@@ -15,15 +15,37 @@ config = {
   ],
 }
 
-# Create group chat, and group chat manager
+user = UserProxyAgent(
+    name="user",
+    system_message="You are the users.",
+    human_input_mode="ALWAYS",
+    code_execution_config={
+        "use_docker": False,
+    },
+)
 
-assistant = AssistantAgent(
-    name="assistant",
-    system_message="You are a helpful assistant.",
-    max_consecutive_auto_reply=2,
+task_master = AssistantAgent(
+    name="Task_Master",
+    system_message="your job is to create an understandable, comprhensive and detailed task",
     llm_config={
-        "timeout": 600,
-        "cache_seed": 42,
+        "config_list": config["config_list"]
+    },
+    human_input_mode="NEVER",
+)
+
+poa_agent_1 = AssistantAgent(
+    name="PoA_Agent_1",
+    system_message="You are an expert in cybersecurity, your task is to analyze the PoA based on the given project requirements.",
+    llm_config={
+        "config_list": config["config_list"]
+    },
+    human_input_mode="NEVER",
+)
+
+poa_agent_2 = AssistantAgent(
+    name="PoA_Agent_2",
+    system_message="You are an expert in cybersecurity, your task is to analyze the PoA based on the given project requirements.",
+    llm_config={
         "config_list": config["config_list"]
     },
     human_input_mode="NEVER",
@@ -33,15 +55,15 @@ sentence_transformer_ef = SentenceTransformer("all-mpnet-base-v2").encode
 client = QdrantClient(url="https://c8edc979-935b-4327-951a-1590a78b6e4a.us-east4-0.gcp.cloud.qdrant.io", api_key=os.getenv("QDRANT_API_KEY"))
 
 ragproxyagent = RetrieveUserProxyAgent(
-    name='rag_proxy_agent',
-    human_input_mode="NEVER",
-    max_consecutive_auto_reply=2,
+    name='RAG_Agent',
+    human_input_mode="ALWAYS",
     retrieve_config={
         "task": "default",
         "docs_path": [
-          # "Ansari et al. - 2022 - STORE Security Threat Oriented Requirements Engineering Methodology.pdf",
+          #"Ansari et al. - 2022 - STORE Security Threat Oriented Requirements Engineering Methodology.pdf",
+          #"PoA Guideline.pdf"
         ],  # change this to your own path, such as https://raw.githubusercontent.com/ag2ai/ag2/main/README.md
-        "chunk_token_size": 10000,
+        # "chunk_token_size": 10000,
         "context_max_tokens": 10000,
         "must_break_at_empty_line": False,
         "model": config["config_list"][0]["model"],
@@ -52,4 +74,50 @@ ragproxyagent = RetrieveUserProxyAgent(
         "embedding_function": sentence_transformer_ef,  # If left out fastembed "BAAI/bge-small-en-v1.5" will be used
     },
     code_execution_config=False,
+)
+
+def bsaa_speaker_selection_func(last_speaker: Agent, groupchat: GroupChat):
+    """Define a customized speaker selection function.
+    A recommended way is to define a transition for each speaker in the groupchat.
+
+    Returns:
+        Return an `Agent` class or a string from ['auto', 'manual', 'random', 'round_robin'] to select a default method to use.
+    """
+    messages = groupchat.messages
+
+    # We'll start with a transition to the planner
+    if len(messages) <= 1:
+        return task_master
+    
+    elif last_speaker is user:
+        if "APPROVED" in messages[-2]["content"]:
+            return poa_agent_1
+        else:
+            return task_master
+    
+    # elif last_speaker is ragproxyagent:
+    #     return task_master
+    
+    elif last_speaker is task_master:
+        return user
+
+    elif last_speaker is poa_agent_1:
+        return poa_agent_2
+
+    elif last_speaker is poa_agent_2:
+        return user
+
+    else:
+        return "auto"
+
+poaGroup = GroupChat(
+    agents=[user ,ragproxyagent, task_master, poa_agent_1 ,poa_agent_2],
+    speaker_selection_method=bsaa_speaker_selection_func,
+    messages=[],
+    max_round=20,
+)
+
+chatManager = GroupChatManager(
+    groupchat=poaGroup,
+    system_message="You are the chat manager.",
 )
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from agents import assistant, ragproxyagent
+from agents import chatManager, ragproxyagent, poa_agent_1, task_master, user, poa_agent_2
 import dotenv
 
 def main():
     dotenv.load_dotenv()
-    assistant.reset()
+    poa_agent_1.reset()
+    chatManager.reset()
+    task_master.reset()
+    ragproxyagent.reset()
+    user.reset()
+    poa_agent_2.reset()
 
     user_input = input("Please enter your question: ")
-    qa_problem = user_input
-    chat_results = ragproxyagent.initiate_chat(assistant, message=ragproxyagent.message_generator, problem=qa_problem)
+    inputPrompt = user_input
+    # chat_results = ragproxyagent.initiate_chat(chatManager, message=ragproxyagent.message_generator, problem=inputPrompt)
+    chat_results = user.initiate_chat(chatManager, message=inputPrompt)
     print(chat_results)
 
 if __name__ == "__main__":
